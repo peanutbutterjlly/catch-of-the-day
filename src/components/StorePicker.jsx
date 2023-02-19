081ae4ae20176177ead84369daaa722d61b8b350
peanutbutterjlly <steverios_sr@yahoo.com>
2023-02-19 12:59:21 -0600

    added store selector button and wired it in to the router

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 export default class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" defaultValue={getFunName()} placeholder="Store Name" ref={this.myInput} required />
         <button type="submit">Visit Store →</button>
       </form>
     );
