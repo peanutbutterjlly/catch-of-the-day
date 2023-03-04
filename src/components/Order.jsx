df933ad05e4f68676420aebc91f2b41ec47a15d4
peanutbutterjlly <steverios_sr@yahoo.com>
2023-03-04 12:38:39 -0600

    orders saved to local storage

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -6,6 +6,7 @@ export default class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
+    if (!fish) return null; // Make sure the fish is loaded before we continue
     if (!isAvailable) {
       return (
         <li key={key}>
